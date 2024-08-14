64096f106e4b56f89eae03de284daa1479d20821
Evgeniya-Kr <jenya-kr@yandex.ru>
2024-08-14 07:53:30 +0700

    Add files via upload

--- Динамическая типизация Тетерева ЕН.py
+++ Динамическая типизация Тетерева ЕН.py
@@ -1,7 +1,8 @@
 name = "Евгения Тетерева"
-print(name)
-age = '43'
-print(age)
-print(int(age) + 1)
-is_student = 'True'
-print('is student:', is_student)
+print('Name: ', name)
+age = 43
+print('Age: ', age)
+age = age + 1
+print('New age: ', age)
+is_student = True
+print('is student: ', is_student)
